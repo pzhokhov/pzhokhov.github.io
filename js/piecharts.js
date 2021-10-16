9df7e0ee2c21e955663e78c5b79347d04c125b65
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-10-16 00:11:50 +0000

    Deploy pzhokhov/pzhokhov.github.io to github.com/pzhokhov/pzhokhov.github.io.git:master

--- js/piecharts.js
+++ js/piecharts.js
@@ -7,9 +7,9 @@ function drawChartBuilderRecent() {
     var data = google.visualization.arrayToDataTable([
       ['Builder', 'Hours'],
       
-        [ "Sasha", 17.0 ],
+        [ "Sasha", 19.9 ],
       
-        [ "Peter", 14.0 ],
+        [ "Peter", 16.4 ],
       
         [ "Peter b", 1.5 ],
       
@@ -20,7 +20,7 @@ function drawChartBuilderRecent() {
     ]);
 
     var options = {
-      title: "Build hours  in last 30 days by builder (total 35)",
+      title: "Build hours  in last 30 days by builder (total 40)",
       is3D: true
     };
 
@@ -33,9 +33,9 @@ function drawChartBuilder() {
     var data = google.visualization.arrayToDataTable([
       ['Builder', 'Hours'],
       
-        [ "Peter", 489.79999999999984 ],
+        [ "Peter", 492.1999999999998 ],
       
-        [ "Sasha", 434.29999999999984 ],
+        [ "Sasha", 437.1999999999998 ],
       
         [ "Katya", 95.00000000000003 ],
       
@@ -100,7 +100,7 @@ function drawChartBuilder() {
     ]);
 
     var options = {
-      title: "Build hours by builder (total 1169)",
+      title: "Build hours by builder (total 1175)",
       is3D: true
     };
 
@@ -117,7 +117,7 @@ function drawChartSubkit() {
       
         [ "wing", 868.6500000000008 ],
       
-        [ "fuselage", 136.95 ],
+        [ "fuselage", 142.25 ],
       
     ]);
 
