d73d8b7ddf5570c252844123d581c4b543de0081
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-09-18 03:23:31 +0000

    Deploy pzhokhov/pzhokhov.github.io to github.com/pzhokhov/pzhokhov.github.io.git:master

--- js/piecharts.js
+++ js/piecharts.js
@@ -8,7 +8,7 @@ function drawChartBuilderRecent() {
     var data = google.visualization.arrayToDataTable([
       ['Builder', 'Hours'],
       
-        [ "Sasha + Peter", 81.0 ],
+        [ "Sasha + Peter", 73.0 ],
       
         [ "Brandon", 5.0 ],
       
@@ -19,7 +19,7 @@ function drawChartBuilderRecent() {
     ]);
 
     var options = {
-      title: "Build hours  in last 30 days by builder (total 87)",
+      title: "Build hours  in last 30 days by builder (total 79)",
       is3D: true
     };
 
