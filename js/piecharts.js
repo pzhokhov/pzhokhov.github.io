b91fd2b72438369e29cfbe7cec781d8220650b31
Continuous Integration <username@users.noreply.github.com>
2024-05-14 07:09:36 +0000

    Continuous Integration Build Artifacts

--- js/piecharts.js
+++ js/piecharts.js
@@ -8,7 +8,7 @@ function drawChartBuilderRecent() {
     var data = google.visualization.arrayToDataTable([
       ['Builder', 'Hours'],
       
-        [ "Sasha + Peter", 118.0 ],
+        [ "Sasha + Peter", 107.0 ],
       
         [ "Joost", 4.5 ],
       
@@ -17,7 +17,7 @@ function drawChartBuilderRecent() {
     ]);
 
     var options = {
-      title: "Build hours  in last 30 days by builder (total 125)",
+      title: "Build hours  in last 30 days by builder (total 114)",
       is3D: true
     };
 
